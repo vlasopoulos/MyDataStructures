07fdb29f2770ba6ae37c9140a833eb0da8fc4880
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-12 12:36:16 +0200

    added deletion in BinarySearchTree.h and tests

--- Google_tests/BinarySearchTreeTests.cpp
+++ Google_tests/BinarySearchTreeTests.cpp
@@ -27,3 +27,32 @@ TEST(BSTSuite, insertWithVariadicContainsTest){
     EXPECT_EQ(bst->contains(1), false);
     EXPECT_EQ(bst->contains(6), false);
 }
+
+TEST(BSTSuite, removeTest) {
+    auto bst = std::make_unique<myds::BinarySearchTree<int>>();
+    bst->insert(4,3,5,6);
+    EXPECT_EQ(bst->contains(6), true);
+    //test remove leaf
+    bst->remove(6);
+    EXPECT_EQ(bst->contains(6), false);
+    bst->insert(6);
+    //test remove with 1 child
+    bst->remove(5);
+    EXPECT_EQ(bst->contains(5), false);
+    EXPECT_EQ(bst->contains(6), true);
+    bst->insert(5);
+    bst->insert(7);
+    //test remove with 2 children
+    bst->remove(6);
+    EXPECT_EQ(bst->contains(5), true);
+    EXPECT_EQ(bst->contains(7), true);
+    EXPECT_EQ(bst->contains(6), false);
+}
+
+TEST(BSTSuite, removeNotFoundTest) {
+    testing::internal::CaptureStderr();
+    auto bst = std::make_unique<myds::BinarySearchTree<int>>();
+    bst->insert(4, 3, 5, 6);
+    bst->remove(2);
+    EXPECT_EQ(testing::internal::GetCapturedStderr(), "Data not found.\n");
+}
--- MyDataStructures_lib/BinarySearchTree.h
+++ MyDataStructures_lib/BinarySearchTree.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <memory>
+#include <iostream>
 
 namespace myds{
 
     template <typename T>
     struct TreeNode {
+        ~TreeNode(){
+            std::cout << "deleting node with data " << data << std::endl;
+        }
         T data;
         std::shared_ptr<TreeNode<T>> leftChild = nullptr;
         std::shared_ptr<TreeNode<T>> rightChild = nullptr;
@@ -45,6 +49,55 @@ namespace myds{
         void insert(const T& data, const Ts& ... ts) {
             insert(data);
             insert(ts...);
+        }
+
+        void remove(const T& data) {
+            std::shared_ptr<TreeNode<T>> parentNode = nullptr;
+            auto currentNode = root;
+            while (currentNode != nullptr && currentNode->data != data) {
+                parentNode = currentNode;
+                if (data < currentNode->data) {
+                    currentNode = currentNode->leftChild;
+                } else {
+                    currentNode = currentNode->rightChild;
+                }
+            }
+            if (currentNode == nullptr) {
+                std::cerr << "Data not found." << std::endl;
+                return;
+            //if node has 0 to 1 children
+            } else if ( currentNode->leftChild == nullptr || currentNode->rightChild == nullptr) {
+                std::shared_ptr<TreeNode<T>> temp;
+                if (currentNode->leftChild == nullptr) {
+                    temp = currentNode->rightChild;
+                } else {
+                    temp = currentNode->leftChild;
+                }
+                if (parentNode == nullptr) {
+                    //we changed the root
+                    return;
+                } else if ( currentNode == parentNode->leftChild) {
+                    parentNode->leftChild = temp;
+                } else {
+                    parentNode->rightChild = temp;
+                }
+            //node has 2 children
+            } else {
+                parentNode = nullptr;
+                std::shared_ptr<TreeNode<T>> temp;
+
+                temp = currentNode->rightChild;
+                while (temp->leftChild != nullptr) {
+                    parentNode = temp;
+                    temp = temp->leftChild;
+                }
+                if (parentNode != nullptr) {
+                    parentNode->leftChild = temp->rightChild;
+                } else {
+                    currentNode->rightChild = temp->rightChild;
+                }
+                currentNode->data = temp->data;
+            }
         }
 
         bool contains(const T& data) const{
